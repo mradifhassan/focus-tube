9ba9d2041965f01e63f302bd0e67f69851f1a321
Muhammad Radif Hassan <m.radifhassan11@gmail.com>
2026-07-10 23:22:50 +0600

    Updated SHM playlist by adding two English Videos

--- src/data.js
+++ src/data.js
@@ -1600,7 +1600,7 @@ Lectured by: Sourov Bijoy`
         videoCount: 12,
         thumbnail: "https://i.ytimg.com/vi/LVE9SjOd0Hw/hqdefault.jpg",
         updatedText: "Updated 1 week ago",
-        videos: ["LVE9SjOd0Hw", "3u_UKd8iTTE", "Hi6Uo6NQ8Zc", "_uqioK_AnbM", "29RYUx1KUu0", "-o7X58xkiXo", "9iWqKmnK3_0", "UhJCLSLHi5M", "bS7SoiY8Gzc", "7B3GW6siIVI", "kB2OYCRCHRA", "9HVn0cLmf3o"]
+        videos: ["LVE9SjOd0Hw", "3u_UKd8iTTE", "Hi6Uo6NQ8Zc", "_uqioK_AnbM", "29RYUx1KUu0", "-o7X58xkiXo", "9iWqKmnK3_0", "UhJCLSLHi5M", "bS7SoiY8Gzc", "7B3GW6siIVI", "kB2OYCRCHRA", "9HVn0cLmf3o", "s5q3Ozq8Yg0", "s5q3Ozq8Yg0"]
       },
 {
         id: "Physics_Thermodynamics_03",
