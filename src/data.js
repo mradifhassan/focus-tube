f4238a864bfd0c568395c1844de06147d941cef3
Muhammad Radif Hassan <m.radifhassan11@gmail.com>
2026-07-18 10:26:31 +0600

    Small typo fixed

--- src/data.js
+++ src/data.js
@@ -1865,7 +1865,7 @@ Lectured by: Maksudul Hossain Jewel`
         id: "-ZbvpldoW8w",
         title: "02. Conservation of Momentum Basic Part 05 | ভরবেগের সংরক্ষণশীলতা পর্ব ০৫ | OnnoRokom Pathshala",
         duration: "17:16",
-        thumbnail: "https://i.ytimg.com/vi/ZbvpldoW8w/hqdefault.jpg",
+        thumbnail: "https://i.ytimg.com/vi/-ZbvpldoW8w/hqdefault.jpg",
         channelId: "UCBA6OI6vEDK13jfoiuX694A",
         channelName: "OnnoRokom Pathshala",
         description: `
